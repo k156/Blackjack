3c4d568bdf410d60de269d0ac50ac1bd0197621d
k156 <jm_91@live.co.kr>
2018-11-12 12:17:51 +0900

    try agian

--- Blackjack.py
+++ Blackjack.py
@@ -6,9 +6,11 @@ class Casting:
             return s
 
 class Deck:
+    deck = [heart1,2,3,4,spade1,2,3,4,diamond1,2,3,4,clover1,2,3,4]
+
     def randomcard ():
         import random
-        deck = [heart1,2,3,4,spade1,2,3,4,diamond1,2,3,4,clover1,2,3,4]
+        ddd
 
 
 
