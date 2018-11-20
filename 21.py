daaab9e2102b0b93fb770398807aac4958e1aa34
eeuoo <lee512joo@gmail.com>
2018-11-20 17:08:32 +0900

    new 11/20

---
+++ 21.py
@@ -0,0 +1,64 @@
+import random
+from functools import reduce
+
+deck = ['sa','s2','s3','s4','s5','s6','s7','s8','s9','s10','sj','sq','sk','ca','c2','c3','c4','c5','c6','c7','c8','c9','c10','cj','cq','ck','ha','h2','h3','h4','h5','h6','h7','h8','h9','h10','hj','hq','hk','da','d2','d3','d4','d5','d6','d7','d8','d9','d10','dj','dq','dk']
+
+class Casting:
+    def to_int(s):
+        if type(s) == str:
+            return int(s.strip())
+        else:
+            return s
+
+    def __str__(s):
+        return s
+
+
+class Card:
+  
+    def __init__(self):
+
+        global deck
+        self.cardlist = []                 
+        self.numberlist = [] 
+
+        random.shuffle(deck)
+        self.card = deck.pop()
+        self.cardlist.append( self.card )
+        
+        num = self.card.lstrip(self.card[0])
+        
+        if num == 'k' or num == 'q' or num == 'j':
+            num = 10
+
+        elif num == 'a':
+            num = 1    
+        
+        else : num = Casting.to_int(num)
+
+        self.numberlist.append(num)
+
+        print(self.cardlist)
+        print(self.numberlist)
+  
+
+    def cardsum(self):
+
+        self.cardsum = reduce(lambda x, y: x + y, self.numberlist)       
+              
+        print(self.cardsum)
+        
+        
+
+class Dealer(Card):
+
+
+
+
+# a.cardlist()
+
+a = Card()
+a.cardsum()
+
+d = Dealer()
+
--- note-a.py
+++ note-a.py
@@ -1,12 +1,41 @@
-class Card:
-    def __init(self, m, n):
-        self.m = m  #모양
-        self.n = n  #숫자
-        
-        cards = [
-            Card('Clover', 3),
-            Card('Spade', 9),
-            Card('Heart', 13),
-            ....
-        ]
+# import random
 
+# class Deck:
+
+#     def card():
+#         deck_m = ['heart', 'spade', 'diamond', 'clover']
+#         deck_s = ['a',2,3,4,5,6,7,8,9,10,'q','k','j']
+        
+#         print([random.choice(deck_m), random.choice(deck_s)])
+        
+     
+# Deck.card()
+
+
+card = ['q1','q2','s11','d1']
+num = []
+
+for i in card:
+    
+   num.append(i.lstirp(i[0]))
+
+print(num)
+
+
+
+# 정민님 card class
+
+# class Card:
+
+#    def __init__(self):
+#        random.shuffle(deck)
+#        self.card = deck.pop()
+
+#        self.cardlist = []
+#        self.cardlist.append(self.card)
+#        print(self.cardlist)
+
+#        self.numberlist = []
+#        self.numberlist.append(Casting.to_int(self.card.lstrip(self.card[0])))
+
+#        print(self.numberlist)
