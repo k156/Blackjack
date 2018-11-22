2753e5c01d0f508833897be94d2f8b841d8dde9e
k156 <jm_91@live.co.kr>
2018-11-22 17:01:21 +0900

    Dd

--- blackjack_final.py
+++ blackjack_final.py
@@ -45,59 +45,22 @@ class Game:
 
 
 class Player(Game):
-    def __init__(self):
+    def has_a():
+        if i == 'a':
+            input_a = input ("a의 값 결정 (1 = 1 입력, 11 = 11 입력) >> ")
+            if input_a == '1':
+                i = 1
+            elif input_a == '11':       
+                i = 11
         
+        self.cardsum += i
+
+
+    def game_play(self):
         super().__init__()
         self.game2()
         
-        for i in self.numberlist:
-                
-            if i == 'a':
-                input_a = input ("a의 값 결정 (1 = 1 입력, 11 = 11 입력) >> ")
-                if input_a == '1':
-                    i = 1
-                elif input_a == '11':       
-                    i = 11
-            
-            self.cardsum += i
-        
 
-        while( self.cardsum < 21): 
-            
-            self.game2()
-
-            for x, i in enumerate(self.numberlist):
-                
-                if len(self.numberlist) >= 2 and x < ( len(self.numberlist) - 1 ): continue 
-
-                if i == 'a':
-                    input_a = input ("a의 값 결정 (1 = 1 입력, 11 = 11 입력) >> ")
-                    if input_a == '1':
-                        i = 1
-                    elif input_a == '11':       
-                        i = 11
-                
-                self.cardsum += i
-  
-            print("플레이어의 카드>>>", self.cardlist) 
-            
-
-            if self.cardsum == 21: 
-                self.player_print()
-                
-            elif self.cardsum > 21:
-                self.player_print()
-
-            elif self.cardsum < 21: 
-                
-                hitorstand = input("Hit 하고 싶으면 1, Stand 하고 싶으면 2를 입력하세요.")
-
-                if hitorstand == '1': 
-                    continue 
-
-                elif hitorstand == '2':  
-                    self.player_print()
-                    break 
 
 
     def player_print(self):
@@ -167,6 +130,37 @@ while(True):
     if start_game == '':
         
         a = Player()
+        a.game_play()
+        for i in a.numberlist:
+            a.has_a()   
+
+        while( a.cardsum < 21): 
+            a.game2()
+        
+            for x, i in enumerate(a.numberlist):
+                if len(a.numberlist) >= 2 and x < ( len(a.numberlist) - 1 ): continue 
+                a.has_a()
+    
+            print("플레이어의 카드>>>", a.cardlist) 
+            
+
+            if a.cardsum == 21: 
+                a.player_print()
+                
+            elif a.cardsum > 21:
+                a.player_print()
+
+            elif a.cardsum < 21: 
+                
+                hitorstand = input("Hit 하고 싶으면 1, Stand 하고 싶으면 2를 입력하세요.")
+
+                if hitorstand == '1': 
+                    continue 
+
+                elif hitorstand == '2':  
+                    a.player_print()
+                    break 
+        a.player_print()
         b = Dealer() 
 
         print("\n승패 결과는???????") 
