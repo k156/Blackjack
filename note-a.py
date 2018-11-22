d60600123aa1104b20feadfcee32dc897ee7c061
eeuoo <lee512joo@gmail.com>
2018-11-22 16:02:50 +0900

    블랙잭 최종

--- note-a.py
+++
@@ -1,8 +0,0 @@
- self.cardsum = reduce(lambda x, y: x + y, self.numberlist)      
-    if x == 'a' or y == 'a':    
-        if (21 - self.cardsum) <= 10:
-            return 11
-        else :
-            return 1
-
-
