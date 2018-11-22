0fa90197e5a941ffda7aa535cf9bd94aa1a712b6
eeuoo <lee512joo@gmail.com>
2018-11-22 17:55:23 +0900

    블랙잭 진짜 최종?

--- blackjack_final.py
+++ blackjack_final.py
@@ -2,6 +2,13 @@ import random
 from functools import reduce
 import os
 
+g_deck = ['♠a','♠2','♠3','♠4','♠5','♠6','♠7','♠8','♠9','♠10','♠j','♠q','♠k',
+        '♣a','♣2','♣3','♣4','♣5','♣6','♣7','♣8','♣9','♣10','♣j','♣q','♣k',
+        '♥a','♥2','♥3','♥4','♥5','♥6','♥7','♥8','♥9','♥10','♥j','♥q','♥k',
+        '◆a','◆2','◆3','◆4','◆5','◆6','◆7','◆8','◆9','◆10','◆j','◆q','◆k']
+
+random.shuffle(g_deck)        
+
 
 class Game:
   
@@ -10,24 +17,14 @@ class Game:
         self.cardlist = []          
         self.numberlist = [] 
         self.cardsum = 0
-
-    
-    @staticmethod
-    def game1():
-        deck = ['♠a','♠2','♠3','♠4','♠5','♠6','♠7','♠8','♠9','♠10','♠j','♠q','♠k',
-                '♣a','♣2','♣3','♣4','♣5','♣6','♣7','♣8','♣9','♣10','♣j','♣q','♣k',
-                '♥a','♥2','♥3','♥4','♥5','♥6','♥7','♥8','♥9','♥10','♥j','♥q','♥k',
-                '◆a','◆2','◆3','◆4','◆5','◆6','◆7','◆8','◆9','◆10','◆j','◆q','◆k']
-        
-        random.shuffle(deck)
-        card = deck.pop()
-        return card
+        global g_deck
         
 
     def game2(self):
-        self.card = Game.game1()
+
+        self.card = g_deck.pop()
         self.cardlist.append(self.card)
-    
+
         self.num = self.card.lstrip(self.card[0])
         
         if self.num == 'k' or self.num == 'q' or self.num == 'j':
@@ -53,6 +50,7 @@ class Player(Game):
         for i in self.numberlist:
                 
             if i == 'a':
+                print("플레이어의 카드>>>", self.cardlist) 
                 input_a = input ("a의 값 결정 (1 = 1 입력, 11 = 11 입력) >> ")
                 if input_a == '1':
                     i = 1
@@ -60,10 +58,12 @@ class Player(Game):
                     i = 11
             
             self.cardsum += i
+   
         
+    def player_play(self):
 
         while( self.cardsum < 21): 
-            
+           
             self.game2()
 
             for x, i in enumerate(self.numberlist):
@@ -71,6 +71,7 @@ class Player(Game):
                 if len(self.numberlist) >= 2 and x < ( len(self.numberlist) - 1 ): continue 
 
                 if i == 'a':
+                    print("플레이어의 카드>>>", self.cardlist) 
                     input_a = input ("a의 값 결정 (1 = 1 입력, 11 = 11 입력) >> ")
                     if input_a == '1':
                         i = 1
@@ -78,9 +79,8 @@ class Player(Game):
                         i = 11
                 
                 self.cardsum += i
-  
-            print("플레이어의 카드>>>", self.cardlist) 
             
+            print("플레이어의 카드>>>", self.cardlist) 
 
             if self.cardsum == 21: 
                 self.player_print()
@@ -89,7 +89,7 @@ class Player(Game):
                 self.player_print()
 
             elif self.cardsum < 21: 
-                
+
                 hitorstand = input("Hit 하고 싶으면 1, Stand 하고 싶으면 2를 입력하세요.")
 
                 if hitorstand == '1': 
@@ -120,14 +120,16 @@ class Dealer(Game):
                         i = 1
             self.cardsum += i
         
+        print("딜러의 카드리스트>>>",self.cardlist)
 
+    def dealer_play(self):
         while( self.cardsum < 17 ):
-            
+        
             self.game2()
             
             for x, i in enumerate(self.numberlist):
                 
-                if len(self.cardlist) > 2 and x < ( len(self.cardlist) - 1 ): continue 
+                if len(self.cardlist) >= 2 and x < ( len(self.cardlist)- 1 ): continue 
                 
                 if i == 'a':
                     if (21 - self.cardsum) <= 10:
@@ -167,8 +169,11 @@ while(True):
     if start_game == '':
         
         a = Player()
-        b = Dealer() 
-
+        b = Dealer()
+        
+        a.player_play()
+        b.dealer_play()
+       
         print("\n승패 결과는???????") 
         over_21(a.cardsum, b.cardsum)
         
